28f51a449e77cc9591a4ce1133e51cb8c45c107a
mehran <60716378+mehranrdk@users.noreply.github.com>
2026-06-08 21:29:34 +0330

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -12,15 +12,15 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/d950/d950.mk)
+$(call inherit-product, device/lge/d958/d958.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := d950
-PRODUCT_NAME := cm_d950
+PRODUCT_DEVICE := d958
+PRODUCT_NAME := cm_d958
 PRODUCT_BRAND := LGE
-PRODUCT_MODEL := LG-D950
+PRODUCT_MODEL := LG-d958
 PRODUCT_MANUFACTURER := lge
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/z_att_us/zee:4.2.2/JDQ39B/D95010n.1391835334:user/release-keys PRIVATE_BUILD_DESC="z_att_us-user 4.2.2 JDQ39B D95010n.1391835334 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/z_att_us/zee:4.2.2/JDQ39B/d95810n.1391835334:user/release-keys PRIVATE_BUILD_DESC="z_att_us-user 4.2.2 JDQ39B d95810n.1391835334 release-keys"
 
 PRODUCT_PACKAGES += Torch
